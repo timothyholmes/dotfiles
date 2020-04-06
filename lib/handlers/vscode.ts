3a58a094391cfe346505484b7b9c250c90d048e5
Tim Holmes <timholmez@gmail.com>
2020-04-06 18:57:19 -0400

    Updated vscode to vscodium

--- lib/handlers/vscode.ts
+++ lib/handlers/vscode.ts
@@ -3,7 +3,7 @@ const Chalk = require('chalk')
 
 function install(): void {
   if (!sh.which('code')) {
-    sh.exec('brew cask reinstall visual-studio-code')
+    sh.exec('brew cask reinstall vscodium')
     return
   } else {
     console.log(Chalk.magenta('vscode already installed'))
@@ -11,8 +11,8 @@ function install(): void {
   
   console.log(Chalk.bgMagenta('syncing vscode configs...'))
 
-  sh.cp(`${process.cwd()}/assets/settings.json`, '~/Library/Application\\ Support/Code/User/')
-  sh.cp(`${process.cwd()}/assets/keybindings.json`, '~/Library/Application\\ Support/Code/User/')
+  sh.cp(`${process.cwd()}/assets/settings.json`, '~/Library/Application\\ Support/VSCodium//User/')
+  sh.cp(`${process.cwd()}/assets/keybindings.json`, '~/Library/Application\\ Support/VSCodium//User/')
   sh.exec('./assets/vscode_extensions.sh')
 }
 
