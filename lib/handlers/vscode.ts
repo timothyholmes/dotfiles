5423839d1b00a54012619089699d6c58f4533be2
Timothy Holmes <tholmes@themeetgroup.com>
2022-02-10 20:54:02 -0500

    updated settings

--- lib/handlers/vscode.ts
+++ lib/handlers/vscode.ts
@@ -12,7 +12,7 @@ function install(): void {
   console.log(Chalk.bgMagenta('syncing vscode configs...'))
 
   sh.cp(`${process.cwd()}/assets/settings.json`, '~/Library/Application\\ Support/Code/User/')
-  sh.cp(`${process.cwd()}/assets/keybindings.json`, '~/Library/Application\\ Support/Code//User/')
+  sh.cp(`${process.cwd()}/assets/keybindings.json`, '~/Library/Application\\ Support/Code/User/')
   sh.exec('./assets/vscode_extensions.sh')
 }
 
